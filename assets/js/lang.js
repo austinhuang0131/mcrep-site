014bc259f63aeb5613db89dc5e671d3541cf9e7e
Spacey <mrspacey1@gmail.com>
2017-11-04 19:48:11 +0000

    closed beta

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -7,7 +7,7 @@
         copyright: 'MCRep 2017',
         register: 'MCRep | Registration',
         login: 'MCRep | Login',
-        profile: 'Your Profile',
+        profile: 'Profile - ',
       }
     }
   })
--- profile/index.php
+++ profile/index.php
@@ -12,21 +12,40 @@ $username_url = urldecode(urlencode(basename(parse_url($_SERVER['REQUEST_URI'], 
     $userrow = mysqli_fetch_array($userlook);
 ?>
 <?php include '../assets/includes/header.php'; ?>
-<section>
-<v-parallax src="../assets/img/background.jpg" height="1000">
-  <v-layout
-    column
-    align-center
-    justify-center
-    class="white--text"
-  >
-    <img src="../assets/img/mcrep.png" alt="MCRep" height="150"><br>
-    <h1 class="white--text mb-2 display-1 text-xs-center" v-text="register"></h1>
-    <h1><?php echo $username_url; ?></h1>
-    <h3><?php echo urldecode($userrow['role']); ?></h3>
-    <p><?php echo $userrow['bio']; ?></p>
-  </v-layout>
-</v-parallax>
-</section>
-
-<?php include '../assets/includes/beta.php'; include '../assets/includes/footer.php'; ?>
+            
+<main>
+<v-container style="padding-bottom: 100px; padding-top: 100px;">
+<v-layout row wrap align-center>
+<v-flex xs2 offset-xs2 md4>
+			<div class="text-xs-center">
+			<h4><strong><?php echo $username_url; ?></strong></h4>
+			<v-avatar size="125px">
+                <img
+                  class="img-circle elevation-7 mb-2"
+                  src="http://www.brandesscadmusrealestate.com/images/team/blank.png"
+                >
+              </v-avatar>
+			<h5 class="text-xs-center"><?php echo urldecode($userrow['role']); ?>
+			<br><v-chip color="green" text-color="white">Online</v-chip></h5>
+			<center><hr width="20%"></center><br>
+			</div>
+			<div class="text-xs-center">
+			<p>Views: <strong>100</strong>
+			<br>Level: <strong>1</strong>
+			<br>Reputation Level:<v-chip color="green" text-color="white">+10</v-chip></p>
+			<v-btn flat icon color="blue lighten-2"><v-icon>thumb_up</v-icon></v-btn>
+    	<v-btn flat icon color="red lighten-2"><v-icon>thumb_down</v-icon></v-btn>
+		</div>
+	</v-flex>
+	<v-flex xs8 md4>
+		<div class="text-xs-center">
+			<p><?php echo $userrow['bio']; ?></p>
+		</div>
+	</v-flex>
+</v-layout>
+</v-container>
+			
+<?php include '../assets/includes/footer.php'; ?>
+ <script src="https://unpkg.com/vue/dist/vue.js"></script>
+ <script src="https://unpkg.com/vuetify/dist/vuetify.js"></script>
+ <script src="../assets/js/lang.js"></script>
